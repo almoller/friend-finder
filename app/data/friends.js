a0844053873c5db74ee97fd5f5e84a4f135ea659
almoller <moller.alan@gmail.com>
2018-05-08 18:19:44 -0600

    HTML Routes working and survey input collected into a variable...

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,34 +4,12 @@ var friends = [
     {
         "name": "Ray Nier",
         "photo": "http://greatruns.com/wp-content/uploads/2017/05/mt-rainier-natl-park.jpeg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         "name": "Rey Mond",
         "photo": "http://greatruns.com/wp-content/uploads/2017/05/mt-rainier-natl-park.jpeg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }
 ];
 
--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 // Friend-Finder
 // Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function() {
+    console.log("Friends App listening on PORT: " + PORT);
+});
